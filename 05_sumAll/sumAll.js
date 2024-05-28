90a76dbab22a177054a8cba72df4c3880bf7b537
Alisher Urazbayev <Alisher1518204240@gmail.com>
2024-05-28 17:39:43 -0400

    Finish exercises 1-5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, lastNumber) {
+  if (
+    typeof firstNumber !== "number" ||
+    typeof lastNumber !== "number" ||
+    firstNumber < 0 ||
+    lastNumber < 0
+  ) {
+    return "ERROR";
+  }
+  let temp = lastNumber;
+  if (firstNumber > lastNumber) {
+    lastNumber = firstNumber;
+    firstNumber = temp;
+  }
+  let sum = 0;
+  for (let i = firstNumber; i <= lastNumber; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
